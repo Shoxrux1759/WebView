e5b785e92ad5a13d2f318e0adbc00da7f20c05ff
Shoxrux1759 <136296494+Shoxrux1759@users.noreply.github.com>
2024-03-03 00:07:41 +0500

    Delete design_pattern/Pattern directory

--- design_pattern/Pattern/src/main/kotlin/Vazifa_1.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val lamp = Lamp()
-    lamp.turnOn()
-    lamp.turnOn()
-    lamp.turnOff()
-    lamp.turnOff()
-    lamp.breakLamp()
-    lamp.turnOn()
-}
--- design_pattern/Pattern/src/main/kotlin/Vazifa_6.kt
+++
@@ -1,7 +0,0 @@
-package patterns
-fun main() {
-    val highAccessImage: Image = ProxyImage("yuklanayotgan_rasm.jpg", 2)
-    val lowAccessImage: Image = ProxyImage("past_resolution.jpg", 1)
-
-    highAccessImage.display()
-}
